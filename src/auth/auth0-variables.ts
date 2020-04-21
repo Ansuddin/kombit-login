094832073cbf4916ae9319ffc56a87c6a60909fe
ansuddin <ansuddin7@gmail.com>
2020-04-21 14:53:11 +0200

    set url

--- src/auth/auth0-variables.ts
+++ src/auth/auth0-variables.ts
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: "sonnenielsen.eu.auth0.com",
   clientId: "dHfmSI3oL3g2PKjNhBZPYR5TGBHA8rnb",
-  callbackUrl: "http://localhost:3333/callback",
+  callbackUrl: "https://wonderful-sinoussi-99f674.netlify.app/callback",
   scope: "openid profile read:message"
 };
