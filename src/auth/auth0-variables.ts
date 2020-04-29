a468b285cf2ad6b514decade929e3f3c32bcddf4
ansuddin <ansuddin7@gmail.com>
2020-04-29 13:56:49 +0200

    audience

--- src/auth/auth0-variables.ts
+++ src/auth/auth0-variables.ts
@@ -2,5 +2,6 @@ export const AUTH_CONFIG = {
   domain: "sonnenielsen.eu.auth0.com",
   clientId: "dHfmSI3oL3g2PKjNhBZPYR5TGBHA8rnb",
   callbackUrl: "https://wonderful-sinoussi-99f674.netlify.app/callback",
-  scope: "openid profile read:message"
+  scope: "openid profile read:message",
+  audience: "https://sonieapi.sonnenielsen.com"
 };
